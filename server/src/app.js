5a2c6a24266fb5f3c8c346d18a44a15255f10cd9
unknown <mauijaloskor@gmail.com>
2025-06-24 12:18:02 +0600

    Update .env

--- server/src/app.js
+++ server/src/app.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://brnad-pal.vercel.app", "http://localhost:3000"],
     credentials: true,
   })
 );
